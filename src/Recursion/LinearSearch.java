f91df163f3ac93d1757f65f1b9b55b8af4276502
JainamZobaliya <jainam.z@somaiya.edu>
2022-07-27 22:05:32 +0530

    Added Recursion-LinearSearch

---
+++ src/Recursion/LinearSearch.java
@@ -0,0 +1,106 @@
+package Recursion;
+
+import java.io.*;
+
+/**
+ * LinearSearch
+ * Problem Statement: To return the index of a key from an array. 
+ * 	Example:
+ * 		Test Case 1: 
+ * 			Input Array: 4 3 8 2
+ * 			Input Key: 8
+ * 			Output: 2
+ *		Test Case 2: 
+ * 			Input Array: 1 2 2 3
+ * 			Input Key: 2
+ * 			Output: 1
+ *		Test Case 3: 
+ * 			Input Array: 5 8 9 6
+ * 			Input Key: 3
+ * 			Output: -1 ... Not Present in the array
+ */
+
+/**
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+
+public class LinearSearch {
+    static int testCaseSize, arr[][], key[];
+
+    public static void main(String[] args) throws IOException  {
+        // Taking array Input from user
+        takeTestCaseInput();
+        for(int i=0; i<testCaseSize; ++i) {
+            // run ith test Case
+            runTestCase(arr[i], key[i]);
+        }
+    }
+
+    private static void takeTestCaseInput() throws IOException  {
+        final InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        final BufferedReader read = new BufferedReader(inputStreamReader);
+        // Taking a valid test Case Size - input
+        do{
+            try{
+                System.out.print("Enter no. Of test Cases: ");
+                testCaseSize = Integer.parseInt(read.readLine());
+            } catch (Exception err) {
+                System.out.println("Error: " + err);
+            }
+        } while(testCaseSize<=0);
+        // Take input for current test case
+        arr = new int[testCaseSize][];
+        key = new int[testCaseSize];
+        for(int i=0; i<testCaseSize; ++i) {
+        	int arrSize = 0;
+	        do{
+	            try{
+	                System.out.print("Enter arrSize of array: ");
+	                arrSize = Integer.parseInt(read.readLine());
+	            } catch (Exception err) {
+	                System.out.println("Error: " + err);
+	            }
+	        } while(arrSize<=0); 
+	        // Instantiation array with the user input arrSize
+	        arr[i] = new int[arrSize];
+	        System.out.print("Enter array elements(space separated): ");
+	        String[] arrInput = read.readLine().split("\s");
+	        // Initializing array with the user array input
+	        for(int j=0; j<arrSize; ++j)
+	            arr[i][j] = Integer.parseInt(arrInput[j]);
+	        System.out.print("Enter the key to be searched: ");
+            key[i] = Integer.parseInt(read.readLine());
+	    }
+    }
+
+    private static void runTestCase(int[] arr, int searchKey) throws IOException  {
+        // Printing the array
+        System.out.print("Array: ");  
+        printArray(arr);
+        // Linear Search
+        int index = linearSearch(arr, 0, searchKey);
+        // Printing the output
+        System.out.println("Search Key: "+searchKey+"\nPosition: "+index);
+        System.out.println("=================================="); 
+    }
+
+    private static int linearSearch(int[] arr, int position, int searchKey) {
+    	// Base Case
+    	if(position > arr.length-1)
+    		return -1;
+    	// Processing Part
+    	if(arr[position] == searchKey)
+    		return position;
+    	// Recursive Relation
+    	return linearSearch(arr, position+1, searchKey);
+    }
+
+    public static void printArray(int[] arr) {
+        // Traversing the array  
+        for(int i=0; i<arr.length; ++i) 
+            System.out.print(arr[i] + " ");  
+        System.out.println();   
+    }
+    
+}
